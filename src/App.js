a5fa64193c657312668261516d0e28c105e2fd6a
Amir <amirhatam136@gmail.com>
2022-07-30 16:35:11 +0200

    init projet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+
+function App() {
+  return (
+    <div>
+      Test
+    </div>
+  );
+}
+
+export default App;
